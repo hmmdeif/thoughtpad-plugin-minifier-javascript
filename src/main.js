556cbb44e0dcd0f77c91fec18cf821c0b32e7c71
David Fisher-Moreau <deiform87@gmail.com>
2014-12-24 15:45:36 +0100

    Move to local thoughtpad obj
    Signed-off-by: David Fisher-Moreau <deiform87@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-var uglify = require('uglify-js'),
-    _thoughtpad;
+var uglify = require('uglify-js');
 
 var init = function (thoughtpad) {
-    _thoughtpad = thoughtpad;
-    _thoughtpad.subscribe("javascript-postcompile-request", compile);
+    thoughtpad.subscribe("javascript-postcompile-request", compile);
 },
 
 compile = function *(obj) {
     if (!obj.contents) return;
 
-    yield _thoughtpad.notify("javascript-postcompile-complete", { contents: uglify.minify(obj.contents, obj.data).code, name: obj.name });
+    yield obj.thoughtpad.notify("javascript-postcompile-complete", { contents: uglify.minify(obj.contents, obj.data).code, name: obj.name });
 };
 
 module.exports = {
